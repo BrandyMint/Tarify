ccbc71bb546dc8b1dee2d6b10c568cecc8dc9bb3
Alexey Illarionov <littlesavage@rambler.ru>
2013-11-28 15:16:12 +0400

    turn eager load off

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,6 +1,6 @@
 Tarify::Application.configure do
   config.cache_classes = true
-  config.eager_load = true
+  config.eager_load = false #TODO: починить
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
