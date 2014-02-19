5558186b7f34111d3455aa5e9ac675e9b09e48e6
tanraya <demerest@gmail.com>
2014-02-20 00:16:47 +0600

    Clean up controller code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
-  helper_method :with_calculation?
-
   def index
     @calculator = Tariffe::Calculator.new(params[:tariffe_calculator])
-    @tariffes   = Tariffe.includes(:bank).active.ordered
-    @tariffes   = @tariffes.with_total_calculation(@calculator) if @calculator.valid?
+    @tariffes   = Tariffe.includes(:bank)
+                         .active
+                         .ordered
+                         .with_total_calculation(@calculator)
   end
 end
