3d0532c08c8f0beac5259e80d4ea5a5632d52ec4
Danil Pismenny <danil@orionet.ru>
2014-02-20 18:22:53 +0400

    Форма на странице тарифа

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :calculator
+
+  private
+
+  def calculator
+    @calculator ||= Tariffe::Calculator.new(params[:tariffe_calculator])
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @calculator = Tariffe::Calculator.new(params[:tariffe_calculator])
     @tariffes   = Tariffe.includes(:bank)
                          .active
                          .ordered
-                         .with_total_calculation(@calculator)
+                         .with_total_calculation(calculator)
   end
 end
